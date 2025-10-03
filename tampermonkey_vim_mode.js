51947cdb75935f3c5500da3de38adab6a7d583ff
Lev Balagurov <lev.balagurov@datadoghq.com>
2025-10-03 18:08:16 +0200

    Bump version to 1.0.18

--- tampermonkey_vim_mode.js
+++ tampermonkey_vim_mode.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Vim Mode for Text Inputs
 // @namespace    http://tampermonkey.net/
-// @version      1.0.17
+// @version      1.0.18
 // @description  Vim-like editing for textareas and inputs
 // @match        *://*/*
 // @updateURL    https://raw.githubusercontent.com/levabala/tampermonkey-vim-mode/refs/heads/main/tampermonkey_vim_mode.js
